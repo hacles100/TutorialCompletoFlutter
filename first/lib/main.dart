468b1f8d272b3d1636309bb3464d4922869332af
hacles100 <haclesrocha200@gmail.com>
2023-02-01 12:24:28 -0100

    good pratices

--- first/lib/main.dart
+++ first/lib/main.dart
@@ -1,54 +1,7 @@
 
-
+import 'package:first/app_widget.dart';
 import 'package:flutter/material.dart';
 
 main(){
   runApp(const MyApp());
-}
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.red 
-      ),
-      home:const HomePage(),
-    );
-  }
-}
-
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-       child: Center(
-        child: GestureDetector(
-        child: Text(
-          'Contador: $counter',
-          ),
-          onTap: () {
-            setState(() {
-               counter++;
-            });
-            print('$counter');
-          },
-         ),
-       ),
-    );
-  }
 }
